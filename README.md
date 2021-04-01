9783987bf1062a9f666a4bb5f664e871f0bcea04
pavel <pavelbuick@icloud.com>
2021-04-02 07:42:39 +0900

    Cleanup

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 
 
 
+## How to run:
 
-1 - What is the time complexity of your implementation?
+`python3 ./main.py`
+
+
+
+## Questions:
+
+#### 1 - What is the time complexity of your implementation?
 
 O(m) - linear
 
-2 - In general (considering other problems), what is a possible disadvantage of a best-time time complexity implementation?
+#### 2 - In general (considering other problems), what is a possible disadvantage of a best-time time complexity implementation?
 
 The implementation can not be parallelized. Using a less-efficient implementation with a double for-loop, we can calculate sums in parallel and then take the smallest one. This is the map-reduce model. The current solution, while being faster, is limited to a single thread.
+
 
--- main.py
+++ main.py
@@ -1,38 +1,17 @@
 
 
-
 def consecutive_sum(n, m, array):
 	min_sum = 0
-	c = 0
-	for i in range(0, m-n):
-		c+=1
-		sum = 0
-		for q in range(0, n):
-			c+=1
-			sum += array[i+q]
-		if( min_sum == 0 or sum < min_sum ):
-			min_sum = sum
-	print( c )
-	return min_sum
-
-
-def consecutive_sum2(n, m, array):
-	min_sum = 0
-	c = 0
 	sum = 0
 	for q in range(0, n):
-		c+=1
 		sum += array[q]
 	min_sum = sum
 
 	for i in range(n, m):
-		c+=1
 		sum -= array[i-n]
 		sum += array[i]
 		if( sum < min_sum ):
 			min_sum = sum
-	
-	print( c )
 	return min_sum
 
 
@@ -40,14 +19,3 @@ if __name__ == "__main__":
 	min_sum = consecutive_sum2( 4, 5, [5,6,2,1,9] )
 	print( min_sum )
 
-
-# (n+1)*(m-n) = n*(m-n)
-
-# 2  3  4
-# 9, 8, 5
-
-
-# 5(3), 5(9) 5(14)
-# m
-
-# best version can't be run in parallel -> double for-loop can be in paralle map-reduce style
